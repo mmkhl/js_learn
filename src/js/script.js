84657f6f384e533f807bf0c90d653845cafae18b
mmkhl <pmisha1014@gmail.com>
2021-07-23 22:37:52 +0600

    New task

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,6 @@
 // Работа с DOM
 // Вывести кнопку с текстом "Привет", при нажатии на неё выводим alert c текстом "Привет Мир!"
 
+document.body.innerHTML = `
+    <button onclick = "alert('Hello world')">Hello</button>
+`
